dfe86e53e6522d63a027dcb48a0f243800a837a4
ademosthene <demos23a@mtholyoke.edu>
2018-11-30 13:57:54 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+// exports an object with a link to database
+module.exports = {
+
+    'url' : 'mongodb://bootcamp:bootcamp2018@ds117111.mlab.com:17111/roulette' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
